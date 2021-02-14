b01df3201e37de44212ffd338bd3c1b1ea58934d
Gao Sun <gaosun.dev@gmail.com>
2021-02-14 17:34:49 +0800

    Setup debug command

--- sync-extensions.js
+++ sync-extensions.js
@@ -1,22 +1,30 @@
 const fs = require('fs');
 const fse = require('fs-extra');
-const child_process = require('child_process');
-
-process.chdir('./extensions/github-vsc');
-child_process.execSync('yarn compile', { stdio: 'inherit' });
-
-process.chdir('../..');
 
 if (fs.existsSync(`./app/dist/extensions/github-vsc`)) {
   fs.rmdirSync(`./app/dist/extensions/github-vsc`, { recursive: true });
 }
+
 fse.copySync('./extensions/github-vsc', './app/dist/extensions/github-vsc', {
+  overwrite: true,
   filter: (src) => !src.includes('node_modules'),
 });
 
-const packageJSON = fs.readFileSync('./app/dist/extensions/github-vsc/package.json');
-const extensions = [{ packageJSON: JSON.parse(packageJSON), extensionPath: 'github-vsc' }];
+fs.watch('./extensions/github-vsc/dist/extension.js', () => {
+  console.log('syncing dist');
+  fse.copySync('./extensions/github-vsc/dist', './app/dist/extensions/github-vsc/dist', {
+    overwrite: true,
+  });
+});
 
-const content = `var githubVSC=${JSON.stringify(extensions)}`;
+fs.watch('./extensions/github-vsc/package.json', () => {
+  console.log('syncing package.json');
+  const packageJSON = fs.readFileSync('./app/dist/extensions/github-vsc/package.json');
+  const extensions = [{ packageJSON: JSON.parse(packageJSON), extensionPath: 'github-vsc' }];
 
-fs.writeFileSync('./app/github-vsc.js', content);
+  const content = `var githubVSC=${JSON.stringify(extensions)}`;
+
+  fs.writeFileSync('./app/github-vsc.js', content);
+});
+
+console.log('watching extension changes');
