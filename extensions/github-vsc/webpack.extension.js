cc1b5949294deb0625ac2375402f42b6f6c9b51d
Gao Sun <gaosun.dev@gmail.com>
2021-03-14 16:56:35 +0800

    Update extension webpack config

--- extensions/github-vsc/webpack.extension.js
+++ extensions/github-vsc/webpack.extension.js
@@ -6,55 +6,62 @@
 const path = require('path');
 const webpack = require('webpack');
 
-/**@type {import('webpack').Configuration}*/
-const config = {
-  target: 'webworker',
-  mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+const getConfig = (_, { mode }) => {
+  const isDevelopment = mode !== 'production';
 
-  entry: './src/extension/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
-  output: {
-    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'extension.js',
-    libraryTarget: 'commonjs2',
-  },
-  devtool: 'nosources-source-map',
-  externals: {
-    vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
-  },
-  resolve: {
-    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    extensions: ['.ts', '.js'],
-    alias: {
-      '@src': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src/core'),
+  /**@type {import('webpack').Configuration}*/
+  const config = {
+    target: 'webworker',
+    mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+
+    entry: './src/extension/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    output: {
+      // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'extension.js',
+      libraryTarget: 'commonjs2',
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader',
-          },
-        ],
+    devtool: 'nosources-source-map',
+    externals: {
+      vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    },
+    resolve: {
+      // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+      extensions: ['.ts', '.js'],
+      alias: {
+        '@src': path.resolve(__dirname, 'src'),
+        '@core': path.resolve(__dirname, 'src/core'),
       },
-      {
-        test: /\.html$/i,
-        use: [
-          {
-            loader: 'raw-loader',
-          },
-        ],
-      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'ts-loader',
+            },
+          ],
+        },
+        {
+          test: /\.html$/i,
+          use: [
+            {
+              loader: 'raw-loader',
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        IS_DEV: isDevelopment,
+      }),
     ],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      IS_DEV: process.env.NODE_ENV !== 'production',
-    }),
-  ],
+  };
+
+  return config;
 };
-module.exports = config;
+
+module.exports = getConfig;
